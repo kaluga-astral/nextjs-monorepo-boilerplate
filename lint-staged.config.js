416ed714c8ad8d96671e5b2a909425393a06a787
Andrey Potyomkin <and_tem@icloud.com>
2023-02-22 09:57:01 +0300

    chore: Настроен lint-stages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,17 +4,35 @@ module.exports = {
     () => 'npm run lint:types --workspace=@example/admin',
     () => 'yaspeller --only-errors',
   ],
-  'admin/**/styles.{js,jsx,ts,tsx}': [
+  'admin/**/styles.{ts,tsx}': [
     'npm run lint:styles --workspace=@example/admin',
   ],
-  'common/**/*.{js,jsx,ts,tsx}': [
-    'npm run lint --workspace=@example/common',
-    'npm run lint:styles --workspace=@example/common',
+
+  'common/shared/**/*.{js,jsx,ts,tsx}': [
+    'npm run lint --workspace=@common/shared',
     () => 'npm run lint:types --workspace=@example/admin',
-    () => 'npm run lint:types --workspace=@example/common',
+    () => 'npm run lint:types --workspace=@common/shared',
+    () => 'npm run lint:types --workspace=@common/data',
+    () => 'npm run lint:types --workspace=@common/modules',
     () => 'yaspeller --only-errors',
   ],
-  'common/**/styles.{js,jsx,ts,tsx}': [
-    'npm run lint:styles --workspace=@example/common',
+  'common/shared/**/styles.{ts,tsx}': [
+    'npm run lint:styles --workspace=@common/shared',
+  ],
+
+  'common/modules/**/*.{js,jsx,ts,tsx}': [
+    'npm run lint --workspace=@common/modules',
+    () => 'npm run lint:types --workspace=@example/admin',
+    () => 'npm run lint:types --workspace=@common/modules',
+    () => 'yaspeller --only-errors',
+  ],
+  'common/modules/**/styles.{ts,tsx}': [
+    'npm run lint:styles --workspace=@common/modules',
+  ],
+
+  'common/data/**/*.{js,jsx,ts,tsx}': [
+    'npm run lint --workspace=@common/data',
+    () => 'npm run lint:types --workspace=@example/admin',
+    () => 'npm run lint:types --workspace=@common/data',
   ],
 };
